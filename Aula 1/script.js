b1a5a95beaedf74806c9ae295cc8cb0fb2d598e7
Camilasousa96 <csilvatst@gmail.com>
2024-08-30 18:29:20 -0300

    Add files via upload

---
+++ Aula 1/script.js
@@ -0,0 +1,7 @@
+console.log("Olá, Mundo!");
+let preco= prompt("Digite um preço:");
+document.write(preco);
+
+let nome = prompt("Digite o seu nome:")
+document.write(" Olá ",nome,". Seja bem-vindx ao site!");
+//Olá, fulana. Seja bem-vindx ao site!
