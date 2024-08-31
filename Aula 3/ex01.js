b20072037f9b068b2b624db109905d667e149af7
Camilasousa96 <csilvatst@gmail.com>
2024-08-30 21:30:17 -0300

    Add files via upload

---
+++ Aula 3/ex01.js
@@ -0,0 +1,9 @@
+//Declare duas variáveis chamadas "idade" e "peso", 
+//atribua valores a elas e imprimam os valores no console.
+
+let idade=28;
+let peso=54;
+
+console.log("Olá, você tem "+idade+" anos e o seu peso é de "+peso+ "kg");
+//Olá, você tem 28 anos e o seu peso é de 54 kg
+
---
+++ Aula 3/script.js
@@ -0,0 +1,5 @@
+const cidade="Resende";
+console.log(cidade);
+
+//Declare duas variáveis chamadas "idade" e "peso", 
+//atribua valores a elas e imprimam os valores no console.
